c4cfa0b3df76e40df62fb480b258f84dd12b6eff
Carlos Almeida <cealmeida@cijun.sp.gov.br>
2026-02-23 11:35:04 -0300

    Adicionado classificação por força (clonando herois por nº motivos)

--- src/app/components/heroes-classification/heroes-classification.ts
+++ src/app/components/heroes-classification/heroes-classification.ts
@@ -15,11 +15,28 @@ export class HeroesClassification {
 
   ngOnInit(): void {
     this.getHeroes();
+    this.orgHeroes();
   }
 
   getHeroes(): void {
     this.heroesservices.getHeroes()
-      .subscribe(HeroesSelected => this.Hero = HeroesSelected.slice(0, 5));
+      .subscribe(HeroesSelected => this.Hero = HeroesSelected);
   }
-   
+  
+  orgHeroes() {
+    for (let pos = 0; pos < this.Hero.length; pos++) {
+      let save_actual = this.Hero[pos]
+      for (let i = 0; i < 12; i++){
+        
+        if (this.Hero[pos].strengh > this.Hero[i].strengh) {
+          this.Hero[pos] = this.Hero[i];
+          this.Hero[i] = save_actual;
+          console.log(`trocar ${this.Hero[pos].name} pelo ${this.Hero[i].name}`);
+          console.log(`trocar pos: ${pos} pelo i: ${i}?`);
+          i = 0;
+        }
+      }
+    }
+    this.Hero = this.Hero.slice(0, 5);
+  }
 }
